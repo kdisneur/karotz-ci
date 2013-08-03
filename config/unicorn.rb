550003e769d09becbdf43ec9ceb435aeb6eb0bf3
Kevin <kevin@koboyz.org>
2013-08-03 23:49:59 +0200

    Fix problem between unicorn and postgresql

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,3 +10,7 @@ preload_app true
 # Immediately restart any workers that
 # haven't responded within 30 seconds
 timeout 30
+
+before_fork do |server, worker|
+  ActiveRecord::Base.connection.disconnect!
+end
